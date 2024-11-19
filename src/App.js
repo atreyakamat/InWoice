d77ebe2c421124fabeb40cc09f555c4e40275851
Atreya Kamat <atkamat1204@gmail.com>
2024-11-20 05:25:55 +0530

    Initial Commits

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import InvoiceForm from './components/InvoiceForm';
+
+function App() {
+  return (
+    <div className="App">
+      <InvoiceForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // purge: [],
+  // purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  // purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    // enabled: true,
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+}
